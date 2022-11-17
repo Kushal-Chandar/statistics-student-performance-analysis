b9a825de1170bafac0fee1b23a2cd52fa62835e5
Kushal-Chandar <kushalchandariit@gmail.com>
2022-11-17 22:50:31 +0530

    feat: initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+# import dabl
+# import plotly.express as px
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+data = pd.read_csv("StudentsPerformance.csv")
+
+print(data.shape)
+
+data.head()
