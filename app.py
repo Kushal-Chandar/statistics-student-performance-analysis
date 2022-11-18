91aadcf5c74c98c378bdc886ac355cb33960bb67
Kushal-Chandar <kushalchandariit@gmail.com>
2022-11-18 21:01:19 +0530

    feat: ipynb

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-# import dabl
-# import plotly.express as px
-# import seaborn as sns
-# import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import dabl
 
 data = pd.read_csv("StudentsPerformance.csv")
 
-print(data.shape)
+plt.rcParams['figure.figsize'] = (15, 9)
+plt.style.use('tableau-colorblind10')
 
-data.head()
+sns.countplot(data['math score'], palette = 'BuPu')
+plt.title('Comparison of math scores', fontweight = 30, fontsize = 20)
+plt.xlabel('score')
+plt.ylabel('count')
+plt.xticks(rotation = 90)
+plt.show()
